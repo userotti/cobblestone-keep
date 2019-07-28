ae17a33c1467400eaf0b0cc0e8da3aa41e0dd974
Christian Frauenstein <cfrauenstein@gmail.com>
2019-07-28 13:09:57 +0200

    some basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import * as THREE from 'three'
+import { Canvas, useRef } from 'react-three-fiber'
+
+function Blokkie({ position }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <group ref={ref => console.log('we have access to the instance', ref)}>
+      <mesh
+        visible
+        userData={{ test: "hello" }}
+        position={new THREE.Vector3(...position)}
+        rotation={new THREE.Euler(0, 0, 0)}
+        geometry={new THREE.BoxGeometry( 5, 5, 5)}
+        material={new THREE.MeshBasicMaterial({ color: new THREE.Color('indianred'), transparent: true })} />
+    </group>
+  )
+}
+
+
+
+function App() {
+
+  
+  return (
+    <Canvas
+      updateDefaultCamera={false}
+      onCreated={(gl) => {
+
+        const aspect = window.innerWidth / window.innerHeight;
+        const d = 50;
+        
+        let camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 );
+        gl.setDefaultCamera(camera);
+        
+        camera.position.set( 20, 20, 20 ); // all components equal
+        camera.left = -(d * aspect);
+        camera.right = (d * aspect);
+        camera.top = d;
+        camera.bottom = -(d);
+        camera.lookAt( gl.scene.position ); // or the origin
+        camera.updateProjectionMatrix();
+        
+
+      }}
+      >
+      {/* {array.map(()=><Blokkie position={[(Math.random() - Math.random())*100, (Math.random() - Math.random())*100, -10]}/>)} */}
+      <Blokkie position={[0,0,0]}/>
+    </Canvas>
+      
   );
 }
 
