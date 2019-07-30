18d9f54806187f244044a22524f737cd604255cd
Christian Frauenstein <cfrauenstein@gmail.com>
2019-07-30 21:50:48 +0200

    bakstene

--- src/App.js
+++ src/App.js
@@ -1,132 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
 
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { Canvas } from 'react-three-fiber'
 import { useSpring, animated } from 'react-spring/three'
 
 const BLOCK_SIZE = 5;
 const MAP_SIZE = 10;
 
-function createMap(size) {
-  return [...Array(size).keys()].map(()=>{
+function createMap(size, model) {
     return [...Array(size).keys()].map(()=>{
-      return Math.random() > 0.9 ? 1 : 0
+      return [...Array(size).keys()].map(()=>{
+        return Math.random() > 0.8 ? model.clone() : null
+      })
     })
-  })
-}
+  }
 
 function App() {
-
+  const [worldMap, setWorldMap] = useState(null);
   
-  const aspect = window.innerWidth / window.innerHeight;
-  const d = 50;  
+  
 
-  return (
-    <Canvas
-      updateDefaultCamera={false}
-      orthographic={true}
-      camera={{
-        left: - d * aspect,
-        right: d * aspect,
-        top: d,
-        bottom: -d,
-        near: 1,
-        far: 1000
-      }}
-      onCreated={({gl, camera, scene}) => {
+  useEffect(() => {
+    // Instantiate a loader
+    var loader = new GLTFLoader();
+        // Load a glTF resource
+    loader.load(
+      // resource URL
+      '/assets/mure2.gltf',
+      // called when the resource is loaded
+      function ( gltf ) {
 
-        gl.shadowMap.enabled = true;
-        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+        console.log("gltf", gltf);
+        setWorldMap(createMap(10, gltf.scene));
+      },
+      // called while loading is progressing
+      function ( xhr ) {
 
-        camera.position.set( 40, 40, 40 );
-        camera.lookAt( scene.position ); // or the origin  
-        camera.updateProjectionMatrix();
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-        // console.log(gl);
-        
-      }}
-      >
-      {/* <ambientLight intensity={0.5} />
-      <spotLight  
-        intensity={0.3} 
-        position={[0, 70, 80]} 
-        angle={0.6} penumbra={1} 
-        castShadow={true}
-        />
+      },
+      // called when loading has errors
+      function ( error ) {
 
-      <Plane 
-        position={[0+(MAP_SIZE - BLOCK_SIZE)/2,-BLOCK_SIZE/2,0+(MAP_SIZE - BLOCK_SIZE)/2]}
-        />  
-      {createMap(MAP_SIZE).map((column, columnIndex)=>{
-        return column.map((blockPositionValue, rowIndex)=>{
-          return blockPositionValue ? <Blokkie key={columnIndex + '' + rowIndex} position={[-(columnIndex-MAP_SIZE/2) * BLOCK_SIZE,0,-(rowIndex-MAP_SIZE/2) * BLOCK_SIZE]}/> : null
-        })
-      })} */}
+        console.log( 'An error happened', error );
 
-      <Player/>
-      
-    </Canvas>
-      
-  );
+      }
+    );
+
+  }, [setWorldMap])
+ 
+
+  return <GameScene worldMap={worldMap}/>
 }
 
 export default App;
 
 
-
-function Blokkie({ position }) {
-  return (
-    <group >
-      <mesh
-        visible
-        userData={{ test: "hello" }}
-        position={new THREE.Vector3(...position)}
-        rotation={new THREE.Euler(0, 0, 0)}
-        geometry={new THREE.BoxGeometry( BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)}
-        invalidateFrameloop={true}
-        castShadow={true}
-        >
-        <meshStandardMaterial attach="material" color="#886622"/>
-      </mesh>          
-    </group>
-  )
-}
-
-function Plane({ position }) {
-  // Register plane as a physics body with zero mass
-  return (
-    <mesh
-      position={position} 
-      rotation={new THREE.Euler(-Math.PI/2, 0, 0)}
-      receiveShadow={true} 
-    >
-      <planeBufferGeometry 
-        attach="geometry" 
-        args={[BLOCK_SIZE*MAP_SIZE*2, BLOCK_SIZE*MAP_SIZE*2]}/> 
-      <meshStandardMaterial attach="material" color="#272727" />
-    </mesh>
-  )
-}
-
-function Player() {
-  // Register plane as a physics body with zero mass
-  // const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
-  // const happyPress = useKeyPress('h');
-  // console.log("happyPress: ", happyPress);  
-
+function GameScene({ worldMap }) {
+  
+  const aspect = window.innerWidth / window.innerHeight;
+  const d = 50;  
 
   const [position, setPosition] = useState([0, 0, 0]); 
-  
   const [animatedPosition, setAnimatedPosition] = useSpring(() => ({
     position:position
   }));
- 
+
+  
 
   // // Add event listeners
   useEffect(() => {
 
-    // console.log("props:", props);
     // // If pressed key is our target key then set to true
     const downHandler = ({ key }) => {
 
@@ -171,13 +117,104 @@ function Player() {
     };
   }, [position, setAnimatedPosition]); // Empty array ensures that effect is only run on mount and unmount
 
+
+  return (
+    <Canvas
+      updateDefaultCamera={false}
+      orthographic={true}
+      camera={{
+        left: - d * aspect,
+        right: d * aspect,
+        top: d,
+        bottom: -d,
+        near: 1,
+        far: 1000
+      }}
+      onCreated={({gl, camera, scene}) => {
+
+        gl.shadowMap.enabled = true;
+        gl.shadowMap.type = THREE.PCFSoftShadowMap;
+
+        camera.position.set( 40, 40, 40 );
+        camera.lookAt( scene.position ); // or the origin  
+        camera.updateProjectionMatrix();
+
+        // console.log(gl);
+        
+      }}
+      >
+      <ambientLight intensity={0.5} />
+      <spotLight  
+        intensity={0.3} 
+        position={[0, 70, 80]} 
+        angle={0.6} penumbra={1} 
+        castShadow={true}
+        />
+
+      <Plane 
+        position={[0+(MAP_SIZE - BLOCK_SIZE)/2,-BLOCK_SIZE/2,0+(MAP_SIZE - BLOCK_SIZE)/2]}
+        />  
+      {worldMap && worldMap.map((column, columnIndex)=>{
+        return column.map((blockModel, rowIndex)=>{
+          return blockModel ? <Blokkie key={columnIndex + '' + rowIndex} position={[-(columnIndex-MAP_SIZE/2) * BLOCK_SIZE,0,-(rowIndex-MAP_SIZE/2) * BLOCK_SIZE]} muurModel={blockModel}/> : null
+        })
+      })}
+
+      <Player animatedPosition={animatedPosition} size={0.2}/>
+      
+    </Canvas>
+      
+  );
+}
+
+
+function Blokkie({ position, muurModel }) {
+
+  console.log("position: ", position);
+  return (
+    <group>
+      {muurModel && <primitive 
+        object={muurModel.clone()} 
+        position={new THREE.Vector3(...position)} 
+        scale={[BLOCK_SIZE,BLOCK_SIZE,BLOCK_SIZE]}
+        rotation={new THREE.Euler(0, (-Math.PI/2)*Math.floor(Math.random()*5+1), 0)}/>}       
+    </group>
+  )
+  // return (
+  //   <group>
+  //     {muurModel && <mesh
+  //       visible
+  //       userData={{ test: "hello" }}
+  //       position={new THREE.Vector3(...position)}
+  //       rotation={new THREE.Euler(0, 0, 0)}
+  //       geometry={new THREE.BoxGeometry( BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)}
+  //       castShadow={true}
+  //       >
+  //       <meshStandardMaterial attach="material" color="#886622"/>
+  //     </mesh>}       
+  //   </group>
+  // )
+}
+
+function Plane({ position }) {
+  return (
+    <mesh
+      position={position} 
+      rotation={new THREE.Euler(-Math.PI/2, 0, 0)}
+      receiveShadow={true} 
+    >
+      <planeBufferGeometry 
+        attach="geometry" 
+        args={[BLOCK_SIZE*MAP_SIZE*2, BLOCK_SIZE*MAP_SIZE*2]}/> 
+      <meshStandardMaterial attach="material" color="#272727" />
+    </mesh>
+  )
+}
+
+function Player({ animatedPosition, size }) {
   return (
     
     <group >
-      {/* <animated.mesh onClick={e => setActive(!active)} onPointerOver={e => setHover(true)} onPointerOut={e => setHover(false)} {...props}>
-        <octahedronGeometry attach="geometry" />
-        <animated.meshStandardMaterial attach="material" color="grey" />
-      </animated.mesh> */}
       <animated.mesh
         onClick={(e) => {
           
@@ -187,7 +224,7 @@ function Player() {
         userData={{ test: "hello" }}
         position={animatedPosition.position}
         rotation={new THREE.Euler(0, 0, 0)}
-        geometry={new THREE.BoxGeometry( BLOCK_SIZE/1, BLOCK_SIZE/1, BLOCK_SIZE/1)}
+        geometry={new THREE.BoxGeometry( BLOCK_SIZE*size, BLOCK_SIZE*size, BLOCK_SIZE*size)}
         material={new THREE.MeshBasicMaterial({ color: new THREE.Color(0xffffff)})} /> */}
       <animated.pointLight  
         color={0xefef55} 
@@ -195,43 +232,9 @@ function Player() {
         distance={50}
         decay={1}
         castShadow={true}
-        position={animatedPosition}
+        position={animatedPosition.position}
         />
     </group>
   )
 }
 
-
-
-// // Hook
-// function useKeyPress(targetKey) {
-//   // State for keeping track of whether key is pressed
-//   const [keyPressed, setKeyPressed] = useState(false);
-
-//   // If pressed key is our target key then set to true
-//   function downHandler({ key }) {
-//     if (key === targetKey) {
-//       setKeyPressed(true);
-//     }
-//   }
-
-//   // If released key is our target key then set to false
-//   const upHandler = ({ key }) => {
-//     if (key === targetKey) {
-//       setKeyPressed(false);
-//     }
-//   };
-
-//   // Add event listeners
-//   useEffect(() => {
-//     window.addEventListener('keydown', downHandler);
-//     window.addEventListener('keyup', upHandler);
-//     // Remove event listeners on cleanup
-//     return () => {
-//       window.removeEventListener('keydown', downHandler);
-//       window.removeEventListener('keyup', upHandler);
-//     };
-//   }, []); // Empty array ensures that effect is only run on mount and unmount
-
-//   return keyPressed;
-// }
