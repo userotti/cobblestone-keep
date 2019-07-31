5e0e42539fcc10ac05994257bb7ef6df0f8eacad
Christian Frauenstein <cfrauenstein@gmail.com>
2019-07-31 20:26:00 +0200

    fun

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const MAP_SIZE = 10;
 function createMap(size, model) {
     return [...Array(size).keys()].map((itemCol, colIndex)=>{
       return [...Array(size).keys()].map((itemRow, rowIndex)=>{
-        return Math.random() > 0.3 ? {
+        return Math.random() > 0.8 ? {
           model: model.clone(),
           rotation: new THREE.Euler(0, (-Math.PI/2)*Math.floor(Math.random()*5+1), 0),
           position: new THREE.Vector3(...[-(colIndex-MAP_SIZE/2) * BLOCK_SIZE,0,-(rowIndex-MAP_SIZE/2) * BLOCK_SIZE])
@@ -32,12 +32,12 @@ function App() {
         // Load a glTF resource
     loader.load(
       // resource URL
-      '/assets/walls.gltf',
+      '/assets/walls2.gltf',
       // called when the resource is loaded
       function ( gltf ) {
 
         console.log("gltf", gltf);
-        setWorldMap(createMap(10, gltf.scene));
+        setWorldMap(createMap(12, gltf.scene));
       },
       // called while loading is progressing
       function ( xhr ) {
