37168a9ade9a421e26d738b14c166b4d3a9d589f
Christian Frauenstein <cfrauenstein@gmail.com>
2019-08-06 21:35:20 +0200

    wall stuff

--- src/components/three/map/wall.js
+++ src/components/three/map/wall.js
@@ -5,5 +5,14 @@ export default function Wall({ tex_url, px_url, nx_url, py_url, ny_url, pz_url, 
   const [texture] = useMemo(() => new THREE.TextureLoader().load(tex_url), [tex_url])
   
   console.log("texture: ", texture);
-  return null
+  return (
+    <mesh>
+      <planeBufferGeometry 
+        attach="geometry" 
+        args={[1, 1]} />
+      <meshLambertMaterial attach="material" transparent>
+        <primitive attach="map" object={texture} />
+      </meshLambertMaterial>
+    </mesh>
+  )
 }
