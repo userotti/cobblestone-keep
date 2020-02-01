9e0bdfd2bb003d222aae17b5c361fd05a028f353
Christian Frauenstein <cfrauenstein@gmail.com>
2020-02-01 20:31:06 +0200

    removed some crap

--- src/components/three/map/Structural.js
+++ src/components/three/map/Structural.js
@@ -1,14 +1,7 @@
 import React, { Fragment } from 'react'
 import FloorTextureShare from './cells/FloorTextureShare'
-import Floor from './cells/Floor'
-
-import useStore from '../../../store';
 
 export default function Structural() {
-  
-
-  console.log("Structural")
-
 
   return (
     <Fragment>
